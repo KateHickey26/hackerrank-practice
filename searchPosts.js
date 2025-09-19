00ed77ea331b98baeba098f9cbc4f88e1cec5719
KateHickey26 <kate.hickey26@gmail.com>
2025-09-19 14:34:45 +1000

    fetch api posts and search post titles

---
+++ searchPosts.js
@@ -0,0 +1,11 @@
+// Fetch posts from
+// 👉 https://jsonplaceholder.typicode.com/posts
+// Write a function that takes a search term (string) and returns an array of post titles that contain that term (case-insensitive).
+
+async function searchPosts(searchTerm) {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const jsonData = await response.json()
+    return jsonData.filter(post => post.title.includes(searchTerm))
+}
+
+searchPosts("qui").then(console.log)
