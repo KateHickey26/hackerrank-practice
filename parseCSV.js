ea14a799bed01700923d79ebbf22d26dd591ea01
KateHickey26 <kate.hickey26@gmail.com>
2025-10-16 13:53:30 +1100

    add parseCSV file

--- parseCSV.js
+++ parseCSV.js
@@ -1,6 +1,14 @@
 // parse a csv file and return an array of objects
 const fs = require('fs');
-const path = require('path');
-const csv = require('csv-parser');
+
 
 // function to parse CSV
+export function parseCSV(filePath) {
+    const text = fs.readFileSync(filePath, 'utf8');
+    const lines = text.split('\n');
+    if(lines.length === 0) return [];
+
+    const headers = lines[0].split(',').map(h => h.trim());
+    const rows = lines.slice(1);
+    
+}
